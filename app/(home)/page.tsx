32f0ad4755a234f42cfb289d9944c606d4931040
Myungjin Ki <hello@mjki.dev>
2024-11-15 20:14:55 +0900

    Add empty component (#2)

---
+++ app/(home)/page.tsx
@@ -0,0 +1,13 @@
+import Footer from "../components/footer";
+import Header from "../components/header";
+import HomeMain from "./components/home-main";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <HomeMain />
+      <Footer />
+    </div>
+  );
+}
