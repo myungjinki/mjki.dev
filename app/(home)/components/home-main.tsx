033c8e99c35b7031ca855505b58cf679be217f6e
Myungjin Ki <hello@mjki.dev>
2024-11-15 21:40:42 +0900

    Modify title

--- app/(home)/components/home-main.tsx
+++ app/(home)/components/home-main.tsx
@@ -5,7 +5,7 @@ export default function HomeMain() {
     <main className="p-8">
       <div className="flex flex-col items-center gap-4">
         <Image src="/images/profile-1.jpg" alt="profile" width={500} height={500} className="rounded-lg" />
-        <p className="text-2xl">반갑습니다! 저는 기명진입니다.</p>
+        <p className="text-2xl">반갑습니다! 기명진입니다.</p>
       </div>
     </main>
   );
