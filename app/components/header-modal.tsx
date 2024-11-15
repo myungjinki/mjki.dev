b842a56da7ef5329a3418d1f9d648c68fd023d76
Myungjin Ki <hello@mjki.dev>
2024-11-15 23:49:51 +0900

    Add header menu modal

---
+++ app/components/header-modal.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Link from "next/link";
+import React, { useRef, useEffect } from "react";
+
+interface HeaderModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  className?: string;
+}
+
+function HeaderModal({ isOpen, onClose, className = "" }: HeaderModalProps) {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    const dialogElement = dialogRef.current;
+
+    if (dialogElement) {
+      if (isOpen) {
+        dialogElement.showModal();
+      } else {
+        dialogElement.close();
+      }
+
+      const handleBackdropClick = (event: MouseEvent) => {
+        if (event.target === dialogElement) {
+          onClose();
+        }
+      };
+
+      dialogElement.addEventListener("click", handleBackdropClick);
+
+      return () => {
+        dialogElement.removeEventListener("click", handleBackdropClick);
+      };
+    }
+  }, [isOpen, onClose]);
+  return (
+    <dialog ref={dialogRef} className={`h-[calc(100dvh-136px)] mt-[136px] flex flex-col ${className}`}>
+      <Link href="/" className="p-8 border-t-2">
+        Home
+      </Link>
+    </dialog>
+  );
+}
+
+export default HeaderModal;
