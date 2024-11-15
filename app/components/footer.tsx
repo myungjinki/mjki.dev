81123e230386fb02ae8e9cf06f5e543c54650dca
Myungjin Ki <hello@mjki.dev>
2024-11-15 21:17:07 +0900

    Add copyright to the Footer (#2)

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -5,14 +5,20 @@ import { SNS } from "../utils/constants";
 export default function Footer() {
   return (
     <div className="grid grid-cols-4 gap-4 px-8">
-      <div className="col-span-4 name">Myungjin Ki</div>
-      <div className="flex col-span-4 gap-4">
-        <Link href={SNS.GITHUB} target="_blank">
-          <Image src="/svg/github.svg" width={25} height={25} alt="github" />
-        </Link>
-        <Link href={SNS.LINKEDIN} target="_blank">
-          <Image src="/svg/linkedin.svg" width={25} height={25} alt="linkedin" />
-        </Link>
+      <div className="flex flex-col col-span-4 gap-4">
+        <div className="name">Myungjin Ki</div>
+        <div className="flex col-span-4 gap-4">
+          <Link href={SNS.GITHUB} target="_blank">
+            <Image src="/svg/github.svg" width={25} height={25} alt="github" />
+          </Link>
+          <Link href={SNS.LINKEDIN} target="_blank">
+            <Image src="/svg/linkedin.svg" width={25} height={25} alt="linkedin" />
+          </Link>
+        </div>
+      </div>
+      <div className="flex flex-col col-span-4 gap-1 mt-8">
+        <span>All rights reserved</span>
+        <span>© Myungjin Ki 2024</span>
       </div>
     </div>
   );
