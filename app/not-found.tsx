fe201aae6faadf8c67974403b8f80e927316c7c9
Myungjin Ki <hello@mjki.dev>
2024-11-16 19:47:42 +0900

    Modify not-found svg CSS when darkmode is enabled

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function NotFound() {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center dark:invert">
       <Image src="/svg/not-found.svg" alt="404" width={256} height={256} />
     </div>
   );
