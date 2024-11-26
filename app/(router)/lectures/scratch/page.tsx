12592b255a7eff3e2a9f6082f072f2f8ade5bdbf
Myungjin Ki <myungjinki91@gmail.com>
2024-11-26 14:29:09 +0900

    Add scratch lecture

--- app/(router)/lectures/scratch/page.tsx
+++ app/(router)/lectures/scratch/page.tsx
@@ -5,6 +5,7 @@ export default function ScratchPage() {
     <div className="flex flex-col gap-8">
       <Link href="./scratch/1098481551">Avoid arrow</Link>
       <Link href="./scratch/998160557">Polarbear</Link>
+      <Link href="./scratch/998856070">Pencing</Link>
     </div>
   );
 }
